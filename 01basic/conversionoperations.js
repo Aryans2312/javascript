df814876994ad9f3e524345a7b748da6263dad01
Aryans2312 <119020913+Aryans2312@users.noreply.github.com>
2025-01-01 15:17:05 +0530

    Add files via upload

---
+++ 01basic/conversionoperations.js
@@ -0,0 +1,51 @@
+let runs = "283"
+
+console.log(typeof runs)
+
+let valueInNumber = Number(runs)
+
+console.log(typeof valueInNumber)
+
+console.log(valueInNumber)
+
+/*let runs2= "283bc"
+
+console.log(typeof runs)
+
+let value2InNumber = Number(runs2)
+
+console.log(typeof value2InNumber)
+
+console.log(value2InNumber)
+
+let runs3 = Null
+
+console.log(typeof runs3)
+
+let value3InNumber = Number(runs3)
+
+console.log(typeof value3InNumber)
+
+console.log(value3InNumber)
+
+let score = Undefined
+
+console.log(typeof score)
+
+let value4InNumber = Number(score)
+
+console.log(typeof value4InNumber)
+
+console.log(value4InNumber)
+
+let score2 = True
+
+console.log(typeof score2)
+
+let value5InNumber = Number(score2)
+
+console.log(typeof value5InNumber)
+
+console.log(value5InNumber)
+
+*/
