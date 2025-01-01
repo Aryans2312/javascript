c92f8630d4843d0ce999dcd3377c08f96f9d2483
Aryans2312 <119020913+Aryans2312@users.noreply.github.com>
2025-01-01 14:50:59 +0530

    Add files via upload

---
+++ 01basic/datatypes.js
@@ -0,0 +1,30 @@
+"use strict";// this mean to treat all code as the newer js version 
+
+// alert(3+3) // we are using node js and not browser 
+
+console.log(3 + 3)
+
+console.log(3 - 3)
+
+//code readability should be high
+
+let name = "Aryans"
+let age = 21
+let isLoggedIn = true
+
+// number - has range also 2 to the power of 53
+// string 
+//boolean
+//null => standalone value
+//undefined (null and underfined are different)
+//symbol ( useful in react , useful in uniqueness)
+
+// object
+
+
+
+console.log(typeof  age )
+console.log(typeof   Symbol)
+console.log(typeof   isLoggedIn , )
+console.log(typeof   undefined ,)
+console.log(typeof   null)
